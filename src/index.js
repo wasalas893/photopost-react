282777dc42a81f2d606cd6d74ff162fc7da448a9
wasalas <wasalas893@gmail.com>
2019-11-21 22:30:54 +0530

    update the indexjs file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React,{Component} from 'react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ReactDom from 'react-dom';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+//list
+class List extends Component{
+    render(){
+        return(
+            <ol>
+                {this.props.tasks.map((task,index)=> <li key={index}>{task}</li>)}
+            </ol>
+        )
+    }
+
+}
+
+//title
+class Title extends Component{
+    render(){
+    return  <h1>{this.props.title}</h1>
+    }
+}
+
+//main
+class Main extends Component{
+    render(){
+        return <div>
+            <Title title={'toDos'}/>
+            <List tasks={['mow the lawn','walk the dog']}/>
+            <List tasks={['mow the lawn','walk the dog']}/>
+        </div>
+    }
+}
+
+
+
+
+ReactDom.render(<Main/>,document.getElementById('root'));
