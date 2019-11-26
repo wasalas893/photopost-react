ba0f66e93535b5cb33c41cafe255e48e77c9729c
wasalas <wasalas893@gmail.com>
2019-11-26 13:38:20 +0530

    add to the add photo  from

--- src/Components/AddPhoto.js
+++ src/Components/AddPhoto.js
@@ -1,8 +1,26 @@
 import React,{Component} from 'react';
 
 class AddPhoto extends Component {
+
+      handleSubmit(){
+
+      }
+
     render(){
-        return <h1>This is the page add photos</h1>
+        return(
+           <div>
+               <h1>Photopost</h1>
+               <div className="form">
+               <form onSubmit={this.handleSubmit} >
+                   <input type="text" placeholder="Link"/>
+                   <input type="text" placeholder="Descriptions"/>
+                   <button>Post</button>
+               </form>
+               </div>
+           </div>
+
+
+        );
 
     }
 }
